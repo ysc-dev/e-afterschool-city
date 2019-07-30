b0282e193b51de345c42149b3b0fb910e8b9c0a8
hgko <hgko@inspace.re.kr>
2019-07-30 16:26:30 +0900

    초기 설정

---
+++ e-afterschool/src/main/java/com/ysc/afterschool/EAfterschoolApplication.java
@@ -0,0 +1,13 @@
+package com.ysc.afterschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAfterschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAfterschoolApplication.class, args);
+	}
+
+}
